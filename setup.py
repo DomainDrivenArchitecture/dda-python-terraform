437f372fa33c9b76747ec2e589b02b9f51b1502b
jem <michael.jerger@meissa-gmbh.de>
2021-06-25 17:06:41 +0200

    version bump

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ except IOError:
 
 setup(
     name=module_name,
-    version="1.0.1",
+    version="1.0.2-dev",
     url="https://github.com/DomainDrivenArchitecture/python-terraform",
     license="MIT",
     author="Freddy Tan",
