d44570e9bb2be4d793f63827dd96f47c8bd435fe
bom <mattis.boeckle@meissa-gmbh.de>
2022-01-28 09:24:38 +0100

    release

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ except IOError:
 
 setup(
     name=module_name,
-    version="1.0.3-dev",
+    version="1.0.3",
     url="https://github.com/DomainDrivenArchitecture/python-terraform",
     license="MIT",
     author="Freddy Tan",
