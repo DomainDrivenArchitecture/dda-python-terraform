ddb8e4160d98b84847450aed1f20b92a42dc933f
jerger <michael.jerger@meissa-gmbh.de>
2022-08-19 14:11:41 +0200

    release

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ except IOError:
 
 setup(
     name=module_name,
-    version="1.0.4-dev",
+    version="2.0.1",
     url="https://github.com/DomainDrivenArchitecture/python-terraform",
     license="MIT",
     author="Freddy Tan",
